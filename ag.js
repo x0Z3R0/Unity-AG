1380db8f171967fbbfbea4a2b37d72d38d62bec0
x0Z3R0 <juanalvear@hotmail.es>
2016-01-29 13:18:18 +0100

    Create ag.js

---
+++ ag.js
@@ -0,0 +1,73 @@
+#pragma strict
+public var Target : GameObject;
+public var canShoot : boolean = true;
+public var Vectores : double[] = new double[180];
+public var Vectores_scores : double[] = new double[180];
+public var debug : boolean = true;
+public var fallos : long = 0;
+public var aciertos : long = 0;
+public var porc : double = 0f;
+private var shoots : int = 0;
+private var aciertos_100 : int = 0; 
+function Start(){
+	Target = GameObject.Find("Target");
+	var i = 0;
+	while(i < 180){
+		Vectores[i] = 3.5;
+		Vectores_scores[i] = 100000;
+		i++; 
+	}
+}
+
+function CalculatePercentage(){
+	shoots = 0;
+	porc = aciertos_100 / 100.0;
+	aciertos_100 = 0; 
+}
+
+function Update(){
+	if(canShoot){
+		shoots++;
+		Shoot();
+	}
+	if(shoots == 99){
+		CalculatePercentage();
+	}
+}
+function Shoot(){
+	canShoot = false;
+	var desfase_direccion : int = Vector3.Angle(Target.transform.position-transform.position,Target.transform.forward);
+	desfase_direccion = desfase_direccion % 180;
+	gameObject.transform.LookAt(Target.transform);
+	var new_shoot_vector : double = Vectores[desfase_direccion] + (Random.Range(-20,20) / 10.0);
+	transform.eulerAngles.y += new_shoot_vector;
+	yield WaitForSeconds(0.02);
+	var rh : RaycastHit;
+	if(debug)
+		Debug.Log("Angulo: " + desfase_direccion + ";Vector: " + Vectores[desfase_direccion] + ";Nuevo vector: " + new_shoot_vector);
+	
+	if(Physics.Raycast(transform.position,transform.forward,rh,Vector3.Distance(Target.transform.position,transform.position) + 10,Space.Self)){
+		Debug.DrawRay(transform.position,transform.forward * Vector3.Distance(Target.transform.position,transform.position),Color.green,1,Space.Self);
+		if(rh.collider.gameObject.name == Target.name){
+			//Debug.Log("ACIERTO");
+			aciertos++;
+			aciertos_100++;
+			if(Vector3.Distance(Target.transform.position,rh.point) < Vectores_scores[desfase_direccion]){
+				Vectores_scores[desfase_direccion] = Vector3.Distance(Target.transform.position,rh.point);
+				Vectores[desfase_direccion] = new_shoot_vector;
+				if(debug)
+					Debug.Log("Mejora en vector: " + desfase_direccion);
+			}
+		}
+	}else{
+		fallos++;
+		Debug.DrawRay(transform.position,transform.forward * Vector3.Distance(Target.transform.position,transform.position),Color.red,1,Space.Self);
+		var point : Vector3 = transform.forward * Vector3.Distance(Target.transform.position,transform.position);
+		if(Vector3.Distance(point,Target.transform.position)< Vectores_scores[desfase_direccion]){
+			//Debug.Log("FALLO");
+
+			Vectores_scores[desfase_direccion] = Vector3.Distance(Target.transform.position,point);
+			Vectores[desfase_direccion] = new_shoot_vector;
+			if(debug)
+				Debug.Log("Mejora en fallo en vector: " + desfase_direccion);
+		}
